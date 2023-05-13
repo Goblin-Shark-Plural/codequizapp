78565b24efd848fb03f52468a6b673713641fb75
24juice22 <mike.james.nunn@gmail.com>
2023-05-13 11:17:26 -0400

    Boilerplate for server.js updated

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
 const app = express();
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.get('/login', userController.createUser, (req, res) => {
+  console.log('res from session:', res.cookies);
+});
 
-console.log("server is running fast");
+app.get('/questions', (req, res) => {
+  console.log('questions');
+});
+
+console.log('server is running fast');
+
+app.listen(3000);
+
+module.exports = app;
