5fe63f0eaf7170e9f1b160cbe7f80d67094f3bff
Wan Wang <wanwangbtb@gmail.com>
2023-05-15 09:57:50 -0400

    added state for correctCount

--- src/Components/Congrats.jsx
+++ src/Components/Congrats.jsx
@@ -1,13 +1,14 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import Result from './Result.jsx';
-const Congrats = () => {
+const Congrats = ({ correctCount }) => {
+  //todo: pased in correctCount state, also needs questions' LENGTH and prop drill to Result component
   return (
     <div>
       <h2>Congratulations! You Completed All the Tests!</h2>
-      <Result />
+      <Result correctCount={correctCount} />
       <button>Try Again</button>
     </div>
-  )
-}
+  );
+};
 
-export default Congrats
+export default Congrats;
