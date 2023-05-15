a66bfec3b514bf6d8c660e3dfdc2edb57b51de86
Wan Wang <wanwangbtb@gmail.com>
2023-05-15 08:58:49 -0400

    added Congrats component and Result component

---
+++ src/Components/Result.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Result = () => {
+  return (
+    <div>
+      <h2>You did great!</h2>
+      <h2>You have answered 10/15 correctly!</h2>
+    </div>
+  )
+}
+
+export default Result
